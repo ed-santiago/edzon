ad45e21c67bfefa3facb3b058af81cc9b3061092
Edmond <edmond.santiago7@gmail.com>
2024-04-17 14:19:57 +1000

    add css and javascript to have an image slider that slides automatically

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* HEADER NAVBAR */
+
 const hamIcon = document.querySelector("#ham_icon");
 const hamMenuCloseButtont = document.querySelector(".fa-xmark");
 const hamMenu = document.querySelector(".ham_menu");
@@ -9,3 +11,16 @@ hamIcon.addEventListener("click", () => {
 hamMenuCloseButtont.addEventListener("click", () => {
   hamMenu.style.right = "-100vw";
 })
+
+/* HERO SECTION */
+
+const imgList = document.querySelectorAll("#slider img");
+
+let imgListIndex = 0;
+const interval = setInterval(() => {
+  imgList[imgListIndex].scrollIntoView();
+  imgListIndex += 1;
+  if (imgListIndex >= imgList.length) {
+    imgListIndex = 0;
+  }
+}, 3000);
