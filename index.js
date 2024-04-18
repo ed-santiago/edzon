1a9c69312e19dccf5a251797fcbc6652ddc7f959
Edmond <edmond.santiago7@gmail.com>
2024-04-18 15:47:34 +1000

    change some js variable names, refactor html hero section to have divs instead of images so it can hold other tags

--- index.js
+++ index.js
@@ -14,25 +14,21 @@ hamMenuCloseButtont.addEventListener("click", () => {
 
 /* HERO SECTION */
 
-const imgList = document.querySelectorAll("#slider img");
+const divList = document.querySelectorAll("#slider img");
 const sliderNavList = document.querySelectorAll("#slider_nav a");
 const sliderNavArray = Array.from(sliderNavList)
 
-function changeSliderNavScale() {
-  sliderNavArray.forEach((nav, index) => {
-    imgListIndex === index ? nav.style.scale = "1.8" : nav.style.scale = "1";
-  })
-}
-
-let imgListIndex = 0;
+let divIndex = 0;
 const interval = setInterval(() => {
-  imgList[imgListIndex].scrollIntoView();
-  changeSliderNavScale();
-  imgListIndex += 1;
-  if (imgListIndex >= imgList.length) {
-    imgListIndex = 0;
+  divList[divIndex].scrollIntoView();
+  sliderNavArray.forEach((nav, index) => {
+    divIndex === index ? nav.style.scale = "1.8" : nav.style.scale = "1";
+  })
+  divIndex += 1;
+  if (divIndex >= divList.length) {
+    divIndex = 0;
   }
-}, 5000);
+}, 6000);
 
 sliderNavArray.forEach((nav) => {
   nav.addEventListener("click", () => {
