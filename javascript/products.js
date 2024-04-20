1c517f79dac7ae45a16ba151009842f62ddc8df2
Edmond <edmond.santiago7@gmail.com>
2024-04-20 17:12:02 +1000

    add shopping cart icon to each product card

--- javascript/products.js
+++ javascript/products.js
@@ -10,12 +10,18 @@ function renderProducts(products) {
 
 function renderProduct(product) {
   const productDiv = document.createElement("div");
+  productDiv.id = "product_card";
   productDiv.innerHTML = `
     <img src=${product.image} alt="idk">
     <p class="category">${product.category}</p>
     <h2>${product.title}</h2>
-    <p>${starRating(product.rating)}</p>
-    <p class="price">${product.price}</p>
+    <div id=starPriceCart>
+      <div id="star_and_price">
+        ${starRating(product.rating)}
+        <p class="price">$${product.price}</p>
+      </div>
+      <i class="fa-solid fa-cart-shopping"></i>
+    </div>
   `
   productSection.append(productDiv);
 }
