eb69da8c35ca977d4b7b840050caa9a3af93d00b
Edmond <edmond.santiago7@gmail.com>
2024-04-21 20:01:47 +1000

    separate headernav and hero section into its own js file

--- javascript/hero.js
+++ javascript/hero.js
@@ -1,17 +1,3 @@
-/* HEADER NAVBAR */
-
-const hamIcon = document.querySelector("#ham_icon");
-const hamMenuCloseButtont = document.querySelector(".fa-xmark");
-const hamMenu = document.querySelector(".ham_menu");
-
-hamIcon.addEventListener("click", () => {
-  hamMenu.style.right = "0";
-})
-
-hamMenuCloseButtont.addEventListener("click", () => {
-  hamMenu.style.right = "-100vw";
-})
-
 /* HERO SECTION */
 
 const slider = document.querySelector("#slider");
