7130656d1442aa818b7352d299d92e5abc911293
Edmond <edmond.santiago7@gmail.com>
2024-04-21 13:20:06 +1000

    add sort feature

--- javascript/sort.js
+++ javascript/sort.js
@@ -2,70 +2,61 @@
 
 const searchForm = document.querySelector("#form_search");
 const searchBar = document.querySelector("#search_value");
-let productArray = [];
+const formSort = document.querySelector("#form_sort");
+let productsArray = [];
+let sortFilteredProducts = [];
 
 fetch("https://edzon-db.onrender.com/products")
   .then(res => res.json())
-  .then(products => productArray = products);
+  .then(products => productsArray = products);
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const searchValue = searchBar.value.toLowerCase();
-  const filteredProducts = productArray.filter(product => {
+  const filteredProducts = productsArray.filter(product => {
     return product.title.toLowerCase().includes(searchValue) ||
-    product.category.toLowerCase().includes(searchValue)
+      product.category.toLowerCase().includes(searchValue)
   })
+  sortFilteredProducts = filteredProducts;
   renderProducts(filteredProducts);
+  searchForm.reset();
+  formSort.reset();
 })
 
 /* SORT PRODUCTS */
 
-const productSectionChildren = document.querySelector("#product_section").children;
-/* setTimeout(() => {
-  productArray = [...productSectionChildren]
-}, 1000) */
-
-const formSort = document.querySelector("#form_sort");
-
-formSort.addEventListener("change", () => {
-  console.log(idk)
+formSort.addEventListener("change", (e) => {
+  switch (e.target.value) {
+    case "all":
+      renderProducts(productsArray);
+      break;
+    case "low_to_high":
+      renderProducts(lowToHigh());
+      break;
+    case "high_to_low":
+      renderProducts(highToLow());
+      break;
+    case "sale":
+      console.log("sale");
+      break;
+  }
 })
 
-/* fetch("https://edzon-db.onrender.com/products")
-    .then(res => res.json())
-    .then(products => sortProducts(products));
-
-function sortProducts(products) {
-  formSort.addEventListener("change", (e) => {
-    productSection.innerHTML = "";
-    switch (e.target.value) {
-      case "all":
-        renderProducts(products);
-        break;
-      case "low_to_high":
-        renderProducts(lowToHigh(products));
-        break;
-      case "high_to_low":
-        renderProducts(highToLow(products));
-        break;
-      case "sale":
-        console.log("sale");
-        break;
-    }
-  })
+function lowToHigh() {
+  if (sortFilteredProducts.length === 0) {
+    return productsArray.sort((a, b) => a.price - b.price)
+  } else {
+    return sortFilteredProducts.sort((a, b) => a.price - b.price)
+  }
 }
 
-function lowToHigh(products) {
-  return products.sort((a, b) => {
-    return a.price - b.price;
-  })
+function highToLow() {
+  if (sortFilteredProducts.length === 0) {
+    return productsArray.sort((a, b) => b.price - a.price)
+  } else {
+    return sortFilteredProducts.sort((a, b) => b.price - a.price)
+  }
 }
-
-function highToLow(products) {
-  return products.sort((a, b) => {
-    return b.price - a.price;
-  })
-} */
 
 /* return products.sort((a, b) => {
   return a.price - b.price;
